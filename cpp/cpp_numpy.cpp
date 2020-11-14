d7a6210d2b38c83a33d27df798d8a5a57207ffee
egorkonyagin <egorkonyagin@yandex.ru>
2020-11-15 02:13:48 +0300

    added functionality of strided input

--- cpp/cpp_numpy.cpp
+++ cpp/cpp_numpy.cpp
@@ -12,18 +12,18 @@
 // pure C++ code
 // -------------
 
-inline double window_pos(int idx, double dtau_window, int zmin){
+inline double window_pos(int idx, double dtau_window, double zmin){
     return zmin + idx * dtau_window;
 }
 
-void create_filt(std::vector<double>& input, int idx, int zmin, int zmax, int size)
+void create_filt(std::vector<double>& input, int idx, double zrange, int size)
 {
   double dtau_window = 0.1, window_width = 2.0;
-  double winPos = window_pos(idx, dtau_window, zmin);
-  double step = (double)(zmax-zmin)/(double)size;
+  double winPos = window_pos(idx, dtau_window, 0.0);
+  double step = (double)(zrange)/(double)size;
   
   for(int i=0; i<size; i++)
-      input[i] = (double)zmin + step*i;
+      input[i] = 0.0 + step*i;
 
   std::for_each(input.begin(), input.end(), [&winPos, &window_width](double &n){ n=exp(-pow((n-winPos)/window_width, 100.)); });
 }
@@ -34,7 +34,7 @@ namespace py = pybind11;
 
 //py::array_t<double> py_multiply(py::array_t<double, py::array::c_style | py::array::forcecast> array, int idx)
 py::array_t<double> create_data(py::array_t<double, py::array::c_style | py::array::forcecast> array, 
-                                int n_replicas, int size, int zmin, int zmax, int stride)
+                                int n_replicas, int size, double zrange, int stride)
 {
   // allocate std::vector (to pass to the C++ function)
   
@@ -42,17 +42,24 @@ py::array_t<double> create_data(py::array_t<double, py::array::c_style | py::arr
   //create_filt(filt, idx, zmin, zmax, size);
   
   //int n_replicas = 300;
-  std::vector<double> result_vec(size*n_replicas);
+  std::vector<double> result_vec(size * n_replicas * stride);
 
-  
-  for(int i=0; i<n_replicas;i++){
-      create_filt(filt, i, zmin, zmax, size);
-      for(int j=0; j<size; j++){
-          result_vec[i*size + j] =filt[j]; //*(((double*)array.data())+j)*filt[j];
-           //array[j]*filt[j];
+  for(int k=0; k<n_replicas; k++){
+      create_filt(filt, k, zrange, size);
+      for(int i=0; i<stride; i++){
+          for(int j=0; j<size; j++){
+              result_vec[k*stride*size + i*size + j] = *(((double*)array.data())+j)*filt[j];
+          }
       }
   }
-
+  /*
+  for(int i=0; i<n_replicas;i++){
+      create_filt(filt, i, zrange, size);
+      for(int j=0; j<size; j++){
+          result_vec[i*size + j] = *(((double*)array.data())+j)*filt[j];
+      }
+  }
+  */
   // allocate py::array (to pass the result of the C++ function to Python)
   auto result        = py::array_t<double>(result_vec.size());
   auto result_buffer = result.request();
