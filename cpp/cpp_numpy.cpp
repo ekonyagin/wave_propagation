d9fecb6d4394aae913a7931ca515c4234d41b222
egorkonyagin <egorkonyagin@yandex.ru>
2020-11-15 01:04:28 +0300

    added base support of batched input

--- cpp/cpp_numpy.cpp
+++ cpp/cpp_numpy.cpp
@@ -16,59 +16,45 @@ inline double window_pos(int idx, double dtau_window, int zmin){
     return zmin + idx * dtau_window;
 }
 
-void apply_filter(std::vector<double>& z, int idx){
-    double zmin = -15., zmax = 15., dtau_window = 0.1, window_width = 2.0;
-    std::cout << z[0] << std::endl;    
-    //auto print = [](const double& n) { std::cout << " " << n; };
-    double winPos = window_pos(idx, dtau_window, z[0]);
-
-    std::cout << winPos<<std::endl;
-    std::for_each(z.begin(), z.end(), [&winPos, &window_width](double &n){ n=exp(-pow((n-winPos)/window_width, 100.)); });
-    
-    //Filter(dtau_window, window_width, idx, z);
-
-    //std::for_each(z.cbegin(), z.cend(), print);
-    //std::cout << '\n';
-    //return z_exp;
-}
-
-std::vector<double> apply_sliding_window(const std::vector<double>& input, int idx)
+void create_filt(std::vector<double>& input, int idx, int zmin, int zmax, int size)
 {
-  std::vector<double> output(input.size());
+  double dtau_window = 0.1, window_width = 2.0;
+  double winPos = window_pos(idx, dtau_window, zmin);
+  double step = (double)(zmax-zmin)/(double)size;
+  
+  for(int i=0; i<size; i++)
+      input[i] = (double)zmin + step*i;
 
-  for ( size_t i = 0 ; i < input.size() ; ++i )
-    output[i] = static_cast<double>(input[i]);
-
-  apply_filter(output, idx);
-  return output;
+  std::for_each(input.begin(), input.end(), [&winPos, &window_width](double &n){ n=exp(-pow((n-winPos)/window_width, 100.)); });
 }
-
-// ----------------
-// Python interface
-// ----------------
 
 namespace py = pybind11;
 
 // wrap C++ function with NumPy array IO
 
 //py::array_t<double> py_multiply(py::array_t<double, py::array::c_style | py::array::forcecast> array, int idx)
-py::array_t<double> create_data(int idx, int size, int zmin, int zmax)
+py::array_t<double> create_data(py::array_t<double, py::array::c_style | py::array::forcecast> array, 
+                                int n_replicas, int size, int zmin, int zmax, int stride)
 {
   // allocate std::vector (to pass to the C++ function)
-  std::vector<double> array_vec(size);
+  
+  std::vector<double> filt(size);
+  //create_filt(filt, idx, zmin, zmax, size);
+  
+  //int n_replicas = 300;
+  std::vector<double> result_vec(size*n_replicas);
 
-  // copy py::array -> std::vector
-  //std::memcpy(array_vec.data(),array.data(),array.size()*sizeof(double));
   
-  double step = (double)(zmax-zmin)/(double)size;
-  
-  for(size_t i=0; i<size; i++)
-    array_vec[i] = (double)zmin + step*i;
-  // call pure C++ function
-  std::vector<double> result_vec = apply_sliding_window(array_vec, idx);
+  for(int i=0; i<n_replicas;i++){
+      create_filt(filt, i, zmin, zmax, size);
+      for(int j=0; j<size; j++){
+          result_vec[i*size + j] =filt[j]; //*(((double*)array.data())+j)*filt[j];
+           //array[j]*filt[j];
+      }
+  }
 
   // allocate py::array (to pass the result of the C++ function to Python)
-  auto result        = py::array_t<double>(array_vec.size());
+  auto result        = py::array_t<double>(result_vec.size());
   auto result_buffer = result.request();
   double *result_ptr = (double *) result_buffer.ptr;
 
@@ -79,9 +65,9 @@ py::array_t<double> create_data(int idx, int size, int zmin, int zmax)
 }
 
 // wrap as Python module
-PYBIND11_MODULE(example,m)
+PYBIND11_MODULE(np_fft,m)
 {
   m.doc() = "pybind11 example plugin";
 
-  m.def("window_func", &create_data, "Arguments: (idx, size, zmin, zmax)");
+  m.def("window_func", &create_data, "Arguments: (n_replicas - length of slice along Z-axis,\n size - length of a single Z-slice,\n zmin,\n zmax,\n stride - length of one r*phi stripe\n)");
 }
